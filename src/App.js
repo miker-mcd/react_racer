004626af32fea7b1dcc77f849e4d3d9d0b73041a
miker-mcd <michaelrmcd@gmail.com>
2018-05-08 21:28:01 -0700

    Implement initial racer table, strip and player elements

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <table className="racer-table">
+          <tbody>
+            <tr id="player1-strip">
+              <td />
+              <td className="active" />
+              <td />
+              <td />
+            </tr>
+            <tr id="player2-strip">
+              <td />
+              <td />
+              <td className="active" />
+              <td />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
